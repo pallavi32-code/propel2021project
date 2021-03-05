2f14d4572e260f96c10728f5301f155cc6e9a240
pallavi <pallavisasidharantv@gmail.com>
2021-03-05 16:21:00 +0530

    second commit

--- DemoGitApp/src/com/faith/app/DemoApp.java
+++ DemoGitApp/src/com/faith/app/DemoApp.java
@@ -5,6 +5,7 @@ public class DemoApp {
 	public static void main(String[] args) {
 		// Demostration on GitHub-pudh/fetch
 		System.out.println("Welcome to the world of git");
+		System.out.println("Testing for second commit");
 
 	}
 
